9467cc51cfbee0fdfa287d990520067e609d92d9
Sina Nanaqdi <nanaqdi48@gmail.com>
2026-02-18 03:08:00 -0600

    Adding Trips via Admin and Creating View to out list

--- trip/views.py
+++ trip/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
-
+from .models import Trip , Note
 class HomeView(TemplateView):
     template_name = 'trip/index.html'
+    
+def trips_list(request):
+    trips = Trip.objects.all()
+    context = {
+        'trips' : trips
+    }
+    
+    return render(request,'trip/trip_list.html', context)
