c052d103c15e7d31393b3d16c790a4843de9195e
Sina Nanaqdi <nanaqdi48@gmail.com>
2026-02-18 06:20:54 -0600

    Adding Our Login Template page

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'Media/')
+
+LOGIN_REDIRECT_URL = 'trip_list'
