38701aa831162e82ab9eab915dafcacc2f2b0a96
Sina Nanaqdi <nanaqdi48@gmail.com>
2026-02-18 23:02:16 -0600

    Updating and Deleting a trip

--- trip/views.py
+++ trip/views.py
@@ -1,63 +1,91 @@
+from typing import List
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView , CreateView , DetailView , ListView
-# Create your views here.
-from .models import Trip , Note
+from django.views.generic import TemplateView, CreateView, DetailView, ListView, UpdateView, DeleteView
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+from .models import Trip, Note
+
 class HomeView(TemplateView):
     template_name = 'trip/index.html'
-    
-def trips_list(request):
-    trips = Trip.objects.filter(owner = request.user)
-    context = {
-        'trips' : trips
-    }
-    
-    return render(request,'trip/trip_list.html', context)
 
+def trips_list(request):
+    # print(dir(request))
+    trips = Trip.objects.filter(owner=request.user)
+    context = {
+        "trips": trips
+    }
+    return render(request, 'trip/trips_list.html', context)
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
 
 class TripCreateView(CreateView):
     model = Trip
     success_url = reverse_lazy('trip-list')
-    fields = ['city', 'country', 'start_date', 'end_date']
-    # template named model_form.html
-    
+    fields = ["city", "country", "start_date", "end_date"]
+    #template named model_form.html
+
     def form_valid(self, form):
-        # owner field = logged in user
         form.instance.owner = self.request.user
         return super().form_valid(form)
-    
+
 class TripDetailView(DetailView):
     model = Trip
-    
-    # data stored on trip - also have the Notes data
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         trip = context['object']
         notes = trip.notes.all()
         context['notes'] = notes
         return context
-    
-class NoteDetailView(DetailView):
-    model = Note
-    
-    
+
+class TripUpdateView(UpdateView):
+    model = Trip
+    success_url = reverse_lazy('trip-list')
+    fields = ["city", "country", "start_date", "end_date"]
+    #template named model_form.html
+
+class TripDeleteView(DeleteView):
+    model = Trip
+    success_url = reverse_lazy('trip-list')
+
 class NoteListView(ListView):
     model = Note
-    
+
     def get_queryset(self):
-        queryset = Note.object.filter(trip_owner = self.request.user)
+        queryset = Note.objects.filter(trip__owner=self.request.user)
         return queryset
-    
-    
+
+class NoteDetailView(DetailView):
+    model = Note
+
 class NoteCreateView(CreateView):
     model = Note
     success_url = reverse_lazy('note-list')
     fields = "__all__"
-    
+
     def get_form(self):
-        form = super(CreateView , self).get_form()
-        trips = Trip.object.filter(owner=self.request.user)
+        form = super(NoteCreateView, self).get_form()
+        trips = Trip.objects.filter(owner=self.request.user)
         form.fields['trip'].queryset = trips
         return form
-        
-        
+
+class NoteUpdateView(UpdateView):
+    model = Note
+    success_url = reverse_lazy('note-list')
+    fields = "__all__"
+
+    def get_form(self):
+        form = super(NoteUpdateView, self).get_form()
+        trips = Trip.objects.filter(owner=self.request.user)
+        form.fields['trip'].queryset = trips
+        return form
+
+class NoteDeleteView(DeleteView):
+    model = Note
+    success_url = reverse_lazy('note-list')
+    #not make the template - send post requests here
